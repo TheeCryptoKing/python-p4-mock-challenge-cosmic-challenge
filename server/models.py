ec29f28d7e121b71b47faad86c026cee975b4cb2
kwamebrowne <theeecryptoking@gmail.com>
2023-06-21 00:18:39 -0500

    serialization rules mastered

--- server/models.py
+++ server/models.py
@@ -16,6 +16,8 @@ metadata = MetaData(naming_convention=convention)
 
 db = SQLAlchemy(metadata=metadata)
 
+
+# Nullable and Validates makes data safer
 class Scientist(db.Model, SerializerMixin):
     __tablename__= 'scientists'
     
@@ -24,27 +26,34 @@ class Scientist(db.Model, SerializerMixin):
     field_of_study = db.Column(db.String, nullable=False)
     avatar = db.Column(db.String, nullable=False)
     
-    scientist_missons = db.relationship('Mission', back_populates='scientist')
-    
+    # Back population for ForeignKey in scientist table
+    scientist_missions = db.relationship('Mission', back_populates='scientist')
+    # more exact data transfer for scientists tbale and planet table,     # calling planet from mmissions
     planets = association_proxy("scientist_missions", "planet")
-    # calling planet from mmissions
     
-    # not loading any data 
-    created_at = db.Column(db.DateTime, server_default=db.func.now())
-    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+    # works with brand new seed
+    created_at = db.Column(db.DateTime, server_default = db.func.now())
+    # works when updated 
+    updated_at = db.Column(db.DateTime, onupdate = db.func.now())
     
-    # @validates('name','field_of_study')
-    # def validate_string(self, key, name, field_of_study):
-    #     if isinstance(key, str) and len(key) < 1:
-    #         raise ValueError('Fix it stupid')
-    #     return name, field_of_study
-    
-    serialize_only = (
-        "id",
-        "name",
-        "field_of_study",
-        "avatar"
+    # Redone with serialize_rules
+    # Avoiding max recursion a.k.a infinity loops
+    # need visual representaion of tables to see which data is looping
+    # Writing only in to_dict() to find which attributes cause max_recursion 
+    serialize_rules = (
+        "-planets.planet_missions",
+        "-scientist_missions.planet",
+        "-scientist_missions.scientist"
         )
+    
+    # Done intially with Only
+    # serialize_only = (
+    #     "id",
+    #     "name",
+    #     "field_of_study",
+    #     "avatar"
+    #     )
+    # redone with @validates and name and field_of_study with string 
     
     #  or 
     @validates('name')
@@ -71,65 +80,83 @@ class Planet(db.Model, SerializerMixin):
     nearest_star = db.Column(db.String, nullable=False)
     image = db.Column(db.String, nullable=False)
     
+    # Back population fro ForeignKey in planet table
     planet_missions = db.relationship('Mission', back_populates='planet')
-    
-    scientists = association_proxy("planet_missons", "scientist")
     # association proxy line up with missions from planet
+    scientists = association_proxy("planet_missions", "scientist")
     
+    # works with brand new seed
     created_at = db.Column(db.DateTime, server_default=db.func.now())
+    # works when updated 
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
     
-    serialize_only = (
-        "id",
-        "name",
-        "distance_from_earth",
-        "nearest_star",
-        "image"
-        )
-
+    # Redone with serialize_rules, apparently not needed
+    # Need to refrence back population in proper sytntax order with how it calls data and then returns
+    serialize_rules = (
+        "-planet_missions.planet",
+    )
+    
+    # why do i need only here????
+    # Done intially with Only
+    # serialize_only = (
+    #     "id",
+    #     "name",
+    #     "distance_from_earth",
+    #     "nearest_star",
+    #     "image"
+    #     )
     
     # pass
 
 # Intermediary has to be here??? #Vahan says No 
 class Mission(db.Model, SerializerMixin):
     __tablename__ = 'missions'
+#     __table_args__ = (UniqueConstraint("name", "scientist_id", "planet_id"),)
     
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     scientist_id = db.Column(db.Integer, ForeignKey('scientists.id'), nullable=False)
     planet_id = db.Column(db.Integer, ForeignKey('planets.id'), nullable=False)
     
-    scientist = db.relationship('Scientist', back_populates='scientist_missons')
+    scientist = db.relationship('Scientist', back_populates='scientist_missions')
     planet = db.relationship('Planet', back_populates='planet_missions')
     
+    # works with brand new seed
     created_at = db.Column(db.DateTime, server_default=db.func.now())
+    # works when updated 
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
     
-    serialize_only = (
-        "id",
-        "name",
-        "scientist_id",
-        "planet_id"
-        )
+    # Redone with serialize_rules
+    serialize_rules = (
+        "-scientist.scientist_missions",
+        "-planet.planet_missions",
+    )
+    
+    # Done intially with Only
+    # serialize_only = (
+    #     "id",
+    #     "name",
+    #     "scientist_id",
+    #     "planet_id"
+    #     )
     
     @validates('name')
     def validates_name(self, key, name):
-        if not name or len(name) < 1:
+        if not name or len(name) < 0:
             raise ValueError('Fix the Mission Name')
         return name 
     
     @validates('scientist')
     def validates_scientist(self, key, scientist_id):
-        if not scientist_id:
+        if scientist_id:
             raise ValueError('Fix scientist_id')
         return scientist_id
     
     @validates('planet')
     def validates_planet(self, key, planet_id):
-        if not planet_id:
+        if planet_id:
             raise ValueError('Fix planet_id')
         return planet_id
-    
     
     # pass 
 
@@ -139,5 +166,8 @@ class Mission(db.Model, SerializerMixin):
 # practice validations
 # practice seriale_rules
 
+# learned Flask Shell can be typed inside of server 
+
+#Ask micheal why Post isn't working and why need serialize only on Planets,  
 
 # add any models you may need. 
